99588ff880123ba80d305ec36c6f5803be9184ab
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2022-11-06 23:56:53 +0100

    Fixed contact functionality

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,24 +1,9 @@
 <script setup>
     import emailjs from 'emailjs-com';
     import { ref } from 'vue';
-    import axios from 'axios'
+    import { useToast } from "vue-toastification";
 
-    /* const form = ref(null);
-    const inputFieldReset = ref(null);
-
-    const handleSendMail = () => {
-        emailjs.sendForm('service_tpgwqtt', 'template_1tayn0d', form.value, 'eyjHM7vPS1VsYrbD4')
-        .then(() => {
-          alert('Message sent!')
-          inputFieldReset.value = " ";
-        }, (error) => {
-          alert('Message not sent', error);
-        }); 
-    }
-*/
-    const show = () => {
-        console.log(form.value)
-    }
+    const toast = useToast();
 
     const form = ref({
         fullName: '',
@@ -26,17 +11,45 @@
         message: ''
     })
 
+    const handleSendMail = () => {
+        emailjs.send("service_tpgwqtt","template_1tayn0d",{
+            to_name: "emir.zambakovic7@gmail.com",
+            from_name: form.value.email,
+            message: form.value.message,
+        }, 'eyjHM7vPS1VsYrbD4')
+        .then(() => {
+            toast.success('Message sent successfully')
+            form.value.fullName = ''
+            form.value.email = ''
+            form.value.message = ''
+        }, (error) => {
+            toast.error('Oops! Something went wrong, please try again later.')
+        }); 
+    }
+
+    const show = () => {
+        console.log(form.value)
+    }
+/*
+    const formData = new FormData();
+    formData.append('service_id', 'service_tpgwqtt');
+    formData.append('template_id', 'template_1tayn0d');
+    formData.append('user_id', 'eyjHM7vPS1VsYrbD4');
+
     const handleSendMail = async() => {
         const response = await axios.post('https://api.emailjs.com/api/v1.0/email/send', 
+        formData,
         {
-            service_id: 'service_tpgwqtt',
-            template_id: 'template_1tayn0d',
-            user_id: 'eyjHM7vPS1VsYrbD4'
+            Headers:{
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
         }
         )
         console.log(response)
     }
-
+*/
 </script>
 
 <template>
@@ -45,22 +58,22 @@
             <div class="left-side">
                 <div class="input">
                     <label for="full-name">Full name</label>
-                    <input class="full-name" type="text" size="40" v-model="form.fullName" required>
+                    <input class="full-name" type="text" size="35" v-model="form.fullName" required>
                 </div>
                 <div class="input">
                     <label for="email">Email</label>
-                    <input class="email" type="email" size="40" v-model="form.email" required>
+                    <input class="email" type="email" size="35" v-model="form.email" required>
                 </div>
             </div>
             <div class="right-side">
                 <div class="input-textarea">
                     <label class="textarea-label">Your message</label>
-                    <textarea rows="5" cols="40" v-model="form.message" required></textarea>
+                    <textarea rows="5" cols="35" v-model="form.message" required></textarea>
                 </div>
             </div>
             <input class="submit" type="submit" name="send" />
         </form>
-        <button @click="show">show</button>
+    <button @click="show">abc</button>    
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 import vClickOutside from "click-outside-vue3"
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 
 const app = createApp(App)
 
 app.use(router)
 app.use(vClickOutside)
+app.use(Toast)
 
 app.mount('#app')
